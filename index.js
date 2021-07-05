b40246cafe8c214efcdb5d86a34863b5e51dcfcd
DevWeb13 <rambo13010@hotmail.fr>
2021-07-05 19:10:22 +0200

    ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const questionContainer = document.querySelector(".click-event");
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const response = questionContainer.querySelector("p");
+const mouseCircle = document.querySelector(".mouseCircle");
 
 //Lors du clic sur le container on enleve la réponse si elle est affichée
 questionContainer.addEventListener("click", () => {
@@ -26,4 +27,15 @@ btn2.addEventListener("click", (event) => {
   response.classList.add("lose");
   response.classList.remove("win");
   questionContainer.classList.add("question-clicked");
+});
+
+//Déplacement du rond avec la souris, le rond ne peut pas pénetrer le questionContainer
+window.addEventListener("mousemove", (e) => {
+  if (e.pageY < 140) {
+    mouseCircle.style.top = "140px";
+    mouseCircle.style.left = e.pageX + "px";
+  } else {
+    mouseCircle.style.left = e.pageX + "px";
+    mouseCircle.style.top = e.pageY + "px";
+  }
 });
